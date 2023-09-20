072c62e03bc3fa014bd31be0066b51b037cf5551
sudithraj-ca22 <144789904+sudithraj-ca22@users.noreply.github.com>
2023-09-20 11:06:29 +0530

    Update Chellenge 1.1.py

--- Chellenge 1.1.py
+++ Chellenge 1.1.py
@@ -1,30 +1,10 @@
+def fact_rec(n):
+   if n==0 or n==1:
+       return 1
+   else:
+     return n*fact_rec(n-1)
 
-# 1.1 Implement a recursive function to calculate the factorial of a given number
+number =4
+res=fact_rec(number)
 
-def recur_factorial(n):  
-
-   if n == 1:  
-
-       return n  
-
-   else:  
-
-       return n*recur_factorial(n-1)  
-
-# take input from the user  
-
-num = int(input("Enter a number: "))  
-
-# check is the number is negative  
-
-if num < 0:  
-
-   print("Sorry, factorial does not exist for negative numbers")  
-
-elif num == 0:  
-
-   print("The factorial of 0 is 1")  
-
-else:  
-
-   print("The factorial of",num,"is",recur_factorial(num))
+print("The factrorial of {} is {}.". format(number,res))
