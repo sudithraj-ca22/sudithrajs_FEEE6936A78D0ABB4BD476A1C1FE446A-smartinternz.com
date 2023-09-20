a8b7004ca8afae9072b79d1914c184742c5f9442
sudithraj-ca22 <144789904+sudithraj-ca22@users.noreply.github.com>
2023-09-20 10:40:24 +0530

    Update chellenge 1.2.py

--- chellenge 1.2.py
+++ chellenge 1.2.py
@@ -29,9 +29,3 @@ if (year % 400 == 0) and (year % 100 == 0):
 elif (year % 4 ==0) and (year % 100 != 0):
 
     print("{0} is a leap year".format(year))
-
-
-
-# if not divided by both 400 (century year) and 4 (not century year)
-
-# year is not leap yea
